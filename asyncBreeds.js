de40d6b91892a210c6d66dd87400d1819ad25b1c
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-10-25 21:23:22 -0700

    Fixes bug with the data files
    Data files had return characters at the end resulting in none of the tests passing because the comparison text and the texts in the data files were different by one return character which was not getting logged in the test output.

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -1,19 +1,14 @@
 const fs = require('fs');
 
 const breedDetailsFromFile = function(breed, dataHandler) {
-  console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
-    console.log("In readFile's Callback: it has the data.");
-    // ISSUE: Returning from *inner* callback function, not breedDetailsFromFile.
     if (!error)  dataHandler(data);
   });
-  // ISSUE: Attempting to return data out here will also not work.
-  //        Currently not returning anything from here, so breedDetailsFromFile function returns undefined.
 };
 
-// we try to get the return value
-
 const catFunction = data => {  
-  console.log('Return Value: ', data);
+  console.log(data);
 }
-const bombay = breedDetailsFromFile('Bombay', catFunction);
+const bombay = breedDetailsFromFile('bombay', catFunction);
+
+module.exports = breedDetailsFromFile;
