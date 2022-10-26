8b1205cd371cf2caa801a3865f633aa7d591501c
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-10-25 20:23:11 -0700

    Initializes directory as package with mocha testing
    Initilises the directory and sets up test directory along with mocha testing framework and chai assertions for testing.

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -1,16 +1,19 @@
 const fs = require('fs');
 
-const breedDetailsFromFile = function(breed) {
+const breedDetailsFromFile = function(breed, dataHandler) {
   console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
     console.log("In readFile's Callback: it has the data.");
     // ISSUE: Returning from *inner* callback function, not breedDetailsFromFile.
-    if (!error) return data;
+    if (!error)  dataHandler(data);
   });
   // ISSUE: Attempting to return data out here will also not work.
   //        Currently not returning anything from here, so breedDetailsFromFile function returns undefined.
 };
 
 // we try to get the return value
-const bombay = breedDetailsFromFile('Bombay');
-console.log('Return Value: ', bombay); // => will NOT print out details, instead we will see undefined!
+
+const catFunction = data => {  
+  console.log('Return Value: ', data);
+}
+const bombay = breedDetailsFromFile('Bombay', catFunction);
