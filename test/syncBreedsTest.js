002a2cc199308bf337e886b5968f3bd48d796079
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-10-25 20:39:12 -0700

    Adds additional tests for other cases

--- test/syncBreedsTest.js
+++ test/syncBreedsTest.js
@@ -2,15 +2,18 @@ const assert = require('chai').assert;
 const breedDetails = require('../syncBreeds');
 
 describe('#breedDetails', () => {
+  const catBreeds = {
+    'Balinese': "Balinese are curious, outgoing, intelligent cats with excellent communication skills. They are known for their chatty personalities and are always eager to tell you their views on life, love, and what you’ve served them for dinner.",
+    'Bombay': "The golden eyes and the shiny black coat of the Bombay is absolutely striking. Likely to bond most with one family member, the Bombay will follow you from room to room and will almost always have something to say about what you are doing, loving attention and to be carried around, often on his caregiver's shoulder."
+  };
+  const expectedDesc1 = "The golden eyes and the shiny black coat of the Bombay is absolutely striking. Likely to bond most with one family member, the Bombay will follow you from room to room and will almost always have something to say about what you are doing, loving attention and to be carried around, often on his caregiver's shoulder.";
+  const expectedDesc2 = "Balinese are curious, outgoing, intelligent cats with excellent communication skills. They are known for their chatty personalities and are always eager to tell you their views on life, love, and what you’ve served them for dinner.";
+  const bombay = breedDetails('Bombay');
+  const balinese = breedDetails('Balinese');
   it('returns breed details for the Bombay breed', () => {
-    const catBreeds = {
-      'Balinese': "Balinese are curious, outgoing, intelligent cats with excellent communication skills. They are known for their chatty personalities and are always eager to tell you their views on life, love, and what you’ve served them for dinner.",
-      'Bombay': "The golden eyes and the shiny black coat of the Bombay is absolutely striking. Likely to bond most with one family member, the Bombay will follow you from room to room and will almost always have something to say about what you are doing, loving attention and to be carried around, often on his caregiver's shoulder."
-    };
-    // We expect it to be a function, but will it be?
-    const expectedDesc = "The golden eyes and the shiny black coat of the Bombay is absolutely striking. Likely to bond most with one family member, the Bombay will follow you from room to room and will almost always have something to say about what you are doing, loving attention and to be carried around, often on his caregiver's shoulder.";
-
-    const bombay = breedDetails('Bombay');
-    assert.equal(expectedDesc, bombay);
+    assert.equal(expectedDesc1, bombay);
+  });
+  it('returns breed details for the Balinese breed', () => {
+    assert.equal(expectedDesc2, balinese);
   });
 });
